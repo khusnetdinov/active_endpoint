ef80c6ee5140e09d1e05abef20affd33c4efd41d
artem.kunetsov <artem.kuznetsov@activeplatform.com>
2017-05-26 13:14:30 +0300

    Add rspec-its and pry

--- lib/active_endpoint/rails/middleware.rb
+++ lib/active_endpoint/rails/middleware.rb
@@ -1,4 +1,4 @@
-require "active_endpoint/probe"
+require 'active_endpoint/probe_catcher'
 
 module ActiveEndpoint
   module Rails
@@ -8,7 +8,18 @@ module ActiveEndpoint
       end
 
       def call(env)
-        @app.call(env)
+        dup._call(env)
+      end
+
+      def _call(env)
+        start = Time.now
+        status, headers, response = @app.call(env)
+        stop = Time.now
+
+        duration = stop - start
+        # TODO: process here
+
+        [status, headers, response]
       end
     end
   end
