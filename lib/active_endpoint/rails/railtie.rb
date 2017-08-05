82eaec72aac32124d23e4b370cad9a333b8c5548
Marat Khusnetdinov <marat@khusnetdinov.ru>
2017-08-05 18:46:19 +0300

    Updates

--- lib/active_endpoint/rails/railtie.rb
+++ lib/active_endpoint/rails/railtie.rb
@@ -3,7 +3,7 @@ module ActiveEndpoint
     class Railtie < ::Rails::Railtie
       initializer 'active_endpoint.configure_rails_initialization' do |app|
         ActiveSupport.on_load(:active_record) do
-          ActiveRecord::Base.send(:extend, ::Tagable)
+          ActiveRecord::Base.send(:extend, ::ActiveEndpoint::Extentions::ActiveRecord)
         end
 
         app.middleware.insert(0, ActiveEndpoint::Rails::Middleware)
